2a7d1a7ac854ace16f597d40b9ecbe9232659ea7
RohanJacob23 <rohankoshyjacob@gmail.com>
2023-07-31 15:40:31 +0530

    Made songs api dynamic

--- src/app/api/songs/route.ts
+++ src/app/api/songs/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const images = await prisma.unsplashImages.findMany();
   const reversedImages = images.reverse();
